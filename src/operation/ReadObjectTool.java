41cf63ffd2d40c6d37c5a0c50f3c44f5b027a092
thientuoanh <thientuoanh@gmail.com>
2024-08-19 20:30:33 +0700

    Update Testcase file

--- src/operation/ReadObjectTool.java
+++ src/operation/ReadObjectTool.java
@@ -6,20 +6,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import Data.Contans;
+import testCases.ToolExecute;
 
-public class ReadObject 
+public class ReadObjectTool 
 {
-
 	Properties p = new Properties();
- 
+	 
 	public Properties getObjectRepository() throws IOException
 	{
 		// Read object repository file
-		InputStream stream = new FileInputStream(new File(Contans.HomePath + Contans.ObjectName));
+		InputStream stream = new FileInputStream(new File(ToolExecute.txt_SelectObjectFile.getText()));
 		// load all objects
 		p.load(stream);
 		return p;
 	}
-	
+
 }
